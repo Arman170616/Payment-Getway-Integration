57aa523081b28cf9fe9c56b65a1b6e48dfd5f6d0
H.Arman <64675035+Arman170616@users.noreply.github.com>
2021-01-04 17:13:27 +0600

    Update urls.py

--- paymentGetway/urls.py
+++ paymentGetway/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('payment/', include('payment.urls')),
+    path('', include('payment.urls')),
 ]
